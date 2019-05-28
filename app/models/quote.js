d45a2f5b018e03c1b86603dae13ee4891be0037e
Bryce Edmonds <bryce@bryceedmonds.com>
2019-05-27 21:38:14 -0600

    final push

--- app/models/quote.js
+++ app/models/quote.js
@@ -9,7 +9,7 @@ export default class Quote {
   get Template() {
     return `
     <div class="col-12">
-    <a href="#" style="color: white;text-decoration: none">Author:<span><i class="far fa-comment"></i> ${this.author}</span></a>
+    <a href="#" style="color: white;text-decoration: none">Who <i class="far fa-comment"></i>/<i class="fas fa-pen-fancy"></i> it?<span> ${this.author}</span></a>
 				<h2>${this.quote}</h2>
         </div>
         `
